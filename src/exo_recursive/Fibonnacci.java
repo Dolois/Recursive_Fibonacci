03cac0f9473f5e4948906b0df2dbd3063fed4d31
Dolois <dloisel@sfr.fr>
2018-11-03 21:08:00 +0100

    first commit

--- src/exo_recursive/Fibonnacci.java
+++ src/exo_recursive/Fibonnacci.java
@@ -18,7 +18,7 @@ public class Fibonnacci
     			n2 = 1,
     			n3 = 0;
     
-    static void printFibonacci(int count)
+    static void suiteFibonacci(int count)
     {      
        if (count > 0)
        {
@@ -29,7 +29,7 @@ public class Fibonnacci
     	   n2 = n3;      
     	   System.out.print(" " + n3);
     	   
-    	   printFibonacci(count-1);
+    	   suiteFibonacci(count-1);
         }      
     } 
 	public static void main(String[] args) 
@@ -37,6 +37,6 @@ public class Fibonnacci
 		int count = 17;
 		
 		System.out.print(n1 + " " + n2);			// printing 0 and 1      
-		printFibonacci(count-2);					// n-2 because 2 numbers are already printed   
+		suiteFibonacci(count-2);					// n-2 because 2 numbers are already printed   
 	}
 }
